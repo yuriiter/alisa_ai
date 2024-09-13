f97e961df494d98933adfe6d63231e45dc7024e4
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-09-13 04:56:29 +0200

    examples

--- examples.sh
+++ examples.sh
@@ -1,4 +1,6 @@
-node index.js  --message "I'm in a very good mood today\! Reply with smiles:) And let's chat\!" --chat
-node index.js  --message "You should respond in JSON format only, strict\!\! Also analyze the information from the website" --url "https://website.com" --chat
-cat data.json | node index.js  --message "You should respond in JSON format only, strict\!\! Also, include the information from the website and reply accordingly" --url "https://website.com" --temperature 0.1
-node index.js  --message "You should respond in JSON format only, strict\!\!" --url "https://website.com" --chat --api-key $(cat /path/to/key) --base-url "https://altaopenai.com/v1" --model-name "gpt-4o-mini"
+alisa  --message "I'm in a very good mood today\! Reply with smiles:) And let's chat\!" --chat
+alisa  --message "You should respond in JSON format only, strict\!\! Also analyze the information from the website" --url "https://website.com" --chat
+cat data.json | alisa  --message "You should respond in JSON format only, strict\!\! Also, include the information from the website and reply accordingly" --url "https://website.com" --temperature 0.1
+alisa  --message "You should respond in JSON format only, strict\!\!" --url "https://website.com" --chat --api-key $(cat /path/to/key) --base-url "https://altaopenai.com/v1" --model-name "gpt-4o-mini"
+alisa "What's a field in mathematics?"
+cat data.json | alisa --message "Explain the data please"
